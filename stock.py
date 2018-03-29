d13cbd03658dc7ca5f74bdfc52409e96883daaee
chandramohan jagtap <cmjagtap1@gmail.com>
2018-03-29 23:26:35 +0530

    update

--- stock.py
+++ stock.py
@@ -5,16 +5,16 @@ import matplotlib.pyplot as plt
 
 dates = []
 prices = []
+dataset=[]
 
 def getData(filename):
 	with open (filename,'r') as csvfile:
 		csvFileReader = csv.reader(csvfile)
 		next(csvFileReader)
 		for row in csvFileReader:
-			x=dates.append(int(row[2].split('-')[0]))
+			dates.append(int(row[2].split('-')[0]))
 			prices.append(float(row[3]))
-
-
-	return
-
-getData('kotak.csv')
+	dataset = np.array(prices); return (dataset)
+	
+x = getData('kotak.csv')
+print (x)
